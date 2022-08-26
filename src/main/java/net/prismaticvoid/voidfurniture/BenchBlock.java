20f0a4c4135ffe4c85ad587ff6a5f2c25aad5313
Void <theprismaticvoid@gmail.com>
2022-08-26 14:09:28 -0500

    Fix busted loot tables for wall candlesticks

--- src/main/java/net/prismaticvoid/voidfurniture/BenchBlock.java
+++ src/main/java/net/prismaticvoid/voidfurniture/BenchBlock.java
@@ -42,72 +42,76 @@ public class BenchBlock extends HorizontalFacingBlock {
         RIGHT_DIRECTIONS.put(Direction.SOUTH, Direction.WEST);
         RIGHT_DIRECTIONS.put(Direction.WEST, Direction.NORTH);
 
-        VoxelShape shapeBoth = VoxelShapes.empty();
-        shapeBoth = VoxelShapes.union(shapeBoth, VoxelShapes.cuboid(0, 0, 0.0625, 0.0625, 0.625, 0.1875));
-        shapeBoth = VoxelShapes.union(shapeBoth, VoxelShapes.cuboid(0.9375, 0, 0.0625, 1, 0.625, 0.1875));
-        shapeBoth = VoxelShapes.union(shapeBoth, VoxelShapes.cuboid(0.9375, 0, 0.8125, 1, 0.625, 0.9375));
-        shapeBoth = VoxelShapes.union(shapeBoth, VoxelShapes.cuboid(0, 0, 0.8125, 0.0625, 0.625, 0.9375));
-        shapeBoth = VoxelShapes.union(shapeBoth, VoxelShapes.cuboid(0, 0.625, 0.0625, 1, 0.6875, 0.9375));
-        shapeBoth = VoxelShapes.union(shapeBoth, VoxelShapes.cuboid(0, 0.6875, 0.875, 0.0625, 1.4375, 0.9375));
-        shapeBoth = VoxelShapes.union(shapeBoth, VoxelShapes.cuboid(0.9375, 0.6875, 0.875, 1, 1.4375, 0.9375));
-        shapeBoth = VoxelShapes.union(shapeBoth, VoxelShapes.cuboid(0.6875, 0.6875, 0.875, 0.8125, 1.4375, 0.9375));
-        shapeBoth = VoxelShapes.union(shapeBoth, VoxelShapes.cuboid(0.4375, 0.6875, 0.875, 0.5625, 1.4375, 0.9375));
-        shapeBoth = VoxelShapes.union(shapeBoth, VoxelShapes.cuboid(0.1875, 0.6875, 0.875, 0.3125, 1.4375, 0.9375));
-        shapeBoth = VoxelShapes.union(shapeBoth, VoxelShapes.cuboid(0, 1.4375, 0.875, 1, 1.6875, 0.9375));
-        shapeBoth = VoxelShapes.union(shapeBoth, VoxelShapes.cuboid(0.9375, 0.6875, 0.0625, 1, 0.9375, 0.1875));
-        shapeBoth = VoxelShapes.union(shapeBoth, VoxelShapes.cuboid(0.9375, 0.9375, 0.0625, 1, 1, 0.875));
-        shapeBoth = VoxelShapes.union(shapeBoth, VoxelShapes.cuboid(0, 0.6875, 0.0625, 0.0625, 0.9375, 0.1875));
-        shapeBoth = VoxelShapes.union(shapeBoth, VoxelShapes.cuboid(0, 0.9375, 0.0625, 0.0625, 1, 0.875));
+        var vb1 = VoxelShapes.cuboid(0, 0, 0.0625, 0.0625, 0.625, 0.1875);
+        var vb2 = VoxelShapes.cuboid(0.9375, 0, 0.0625, 1, 0.625, 0.1875);
+        var vb3 = VoxelShapes.cuboid(0.9375, 0, 0.8125, 1, 0.625, 0.9375);
+        var vb4 = VoxelShapes.cuboid(0, 0, 0.8125, 0.0625, 0.625, 0.9375);
+        var vb5 = VoxelShapes.cuboid(0, 0.625, 0.0625, 1, 0.6875, 0.9375);
+        var vb6 = VoxelShapes.cuboid(0, 0.6875, 0.875, 0.0625, 1.4375, 0.9375);
+        var vb7 = VoxelShapes.cuboid(0.9375, 0.6875, 0.875, 1, 1.4375, 0.9375);
+        var vb8 = VoxelShapes.cuboid(0.6875, 0.6875, 0.875, 0.8125, 1.4375, 0.9375);
+        var vb9 = VoxelShapes.cuboid(0.4375, 0.6875, 0.875, 0.5625, 1.4375, 0.9375);
+        var vb10 = VoxelShapes.cuboid(0.1875, 0.6875, 0.875, 0.3125, 1.4375, 0.9375);
+        var vb11 = VoxelShapes.cuboid(0, 1.4375, 0.875, 1, 1.6875, 0.9375);
+        var vb12 = VoxelShapes.cuboid(0.9375, 0.6875, 0.0625, 1, 0.9375, 0.1875);
+        var vb13 = VoxelShapes.cuboid(0.9375, 0.9375, 0.0625, 1, 1, 0.875);
+        var vb14 = VoxelShapes.cuboid(0, 0.6875, 0.0625, 0.0625, 0.9375, 0.1875);
+        var vb15 = VoxelShapes.cuboid(0, 0.9375, 0.0625, 0.0625, 1, 0.875);
+
+        VoxelShape shapeBoth = VoxelShapes.union(vb1, vb2, vb3, vb4, vb5, vb6, vb7, vb8, vb9, vb10, vb11, vb12, vb13, vb14, vb15);
 
         SHAPES_BOTH.put(Direction.NORTH, Utils.rotateShape(Direction.NORTH, shapeBoth));
         SHAPES_BOTH.put(Direction.SOUTH, Utils.rotateShape(Direction.SOUTH, shapeBoth));
         SHAPES_BOTH.put(Direction.EAST, Utils.rotateShape(Direction.EAST, shapeBoth));
         SHAPES_BOTH.put(Direction.WEST, Utils.rotateShape(Direction.WEST, shapeBoth));
 
-        VoxelShape shapeLeft = VoxelShapes.empty();
-        shapeLeft = VoxelShapes.union(shapeLeft, VoxelShapes.cuboid(0.9375, 0, 0.0625, 1, 0.625, 0.1875));
-        shapeLeft = VoxelShapes.union(shapeLeft, VoxelShapes.cuboid(0.9375, 0, 0.8125, 1, 0.625, 0.9375));
-        shapeLeft = VoxelShapes.union(shapeLeft, VoxelShapes.cuboid(0, 0.625, 0.0625, 1, 0.6875, 0.9375));
-        shapeLeft = VoxelShapes.union(shapeLeft, VoxelShapes.cuboid(0, 0.6875, 0.875, 0.0625, 1.4375, 0.9375));
-        shapeLeft = VoxelShapes.union(shapeLeft, VoxelShapes.cuboid(0.9375, 0.6875, 0.875, 1, 1.4375, 0.9375));
-        shapeLeft = VoxelShapes.union(shapeLeft, VoxelShapes.cuboid(0.6875, 0.6875, 0.875, 0.8125, 1.4375, 0.9375));
-        shapeLeft = VoxelShapes.union(shapeLeft, VoxelShapes.cuboid(0.4375, 0.6875, 0.875, 0.5625, 1.4375, 0.9375));
-        shapeLeft = VoxelShapes.union(shapeLeft, VoxelShapes.cuboid(0.1875, 0.6875, 0.875, 0.3125, 1.4375, 0.9375));
-        shapeLeft = VoxelShapes.union(shapeLeft, VoxelShapes.cuboid(0, 1.4375, 0.875, 1, 1.6875, 0.9375));
-        shapeLeft = VoxelShapes.union(shapeLeft, VoxelShapes.cuboid(0.9375, 0.6875, 0.0625, 1, 0.9375, 0.1875));
-        shapeLeft = VoxelShapes.union(shapeLeft, VoxelShapes.cuboid(0.9375, 0.9375, 0.0625, 1, 1, 0.875));
+        var vl1 = VoxelShapes.cuboid(0.9375, 0, 0.0625, 1, 0.625, 0.1875);
+        var vl2 = VoxelShapes.cuboid(0.9375, 0, 0.8125, 1, 0.625, 0.9375);
+        var vl3 = VoxelShapes.cuboid(0, 0.625, 0.0625, 1, 0.6875, 0.9375);
+        var vl4 = VoxelShapes.cuboid(0, 0.6875, 0.875, 0.0625, 1.4375, 0.9375);
+        var vl5 = VoxelShapes.cuboid(0.9375, 0.6875, 0.875, 1, 1.4375, 0.9375);
+        var vl6 = VoxelShapes.cuboid(0.6875, 0.6875, 0.875, 0.8125, 1.4375, 0.9375);
+        var vl7 = VoxelShapes.cuboid(0.4375, 0.6875, 0.875, 0.5625, 1.4375, 0.9375);
+        var vl8 = VoxelShapes.cuboid(0.1875, 0.6875, 0.875, 0.3125, 1.4375, 0.9375);
+        var vl9 = VoxelShapes.cuboid(0, 1.4375, 0.875, 1, 1.6875, 0.9375);
+        var vl10 = VoxelShapes.cuboid(0.9375, 0.6875, 0.0625, 1, 0.9375, 0.1875);
+        var vl11 = VoxelShapes.cuboid(0.9375, 0.9375, 0.0625, 1, 1, 0.875);
+
+        VoxelShape shapeLeft = VoxelShapes.union(vl1, vl2, vl3, vl4, vl5, vl6, vl7, vl8, vl9, vl10, vl11);
 
         SHAPES_LEFT.put(Direction.NORTH, Utils.rotateShape(Direction.NORTH, shapeLeft));
         SHAPES_LEFT.put(Direction.SOUTH, Utils.rotateShape(Direction.SOUTH, shapeLeft));
         SHAPES_LEFT.put(Direction.EAST, Utils.rotateShape(Direction.EAST, shapeLeft));
         SHAPES_LEFT.put(Direction.WEST, Utils.rotateShape(Direction.WEST, shapeLeft));
 
-        VoxelShape shapeRight = VoxelShapes.empty();
-        shapeRight = VoxelShapes.union(shapeRight, VoxelShapes.cuboid(0, 0, 0.0625, 0.0625, 0.625, 0.1875));
-        shapeRight = VoxelShapes.union(shapeRight, VoxelShapes.cuboid(0, 0, 0.8125, 0.0625, 0.625, 0.9375));
-        shapeRight = VoxelShapes.union(shapeRight, VoxelShapes.cuboid(0, 0.625, 0.0625, 1, 0.6875, 0.9375));
-        shapeRight = VoxelShapes.union(shapeRight, VoxelShapes.cuboid(0, 0.6875, 0.875, 0.0625, 1.4375, 0.9375));
-        shapeRight = VoxelShapes.union(shapeRight, VoxelShapes.cuboid(0.9375, 0.6875, 0.875, 1, 1.4375, 0.9375));
-        shapeRight = VoxelShapes.union(shapeRight, VoxelShapes.cuboid(0.6875, 0.6875, 0.875, 0.8125, 1.4375, 0.9375));
-        shapeRight = VoxelShapes.union(shapeRight, VoxelShapes.cuboid(0.4375, 0.6875, 0.875, 0.5625, 1.4375, 0.9375));
-        shapeRight = VoxelShapes.union(shapeRight, VoxelShapes.cuboid(0.1875, 0.6875, 0.875, 0.3125, 1.4375, 0.9375));
-        shapeRight = VoxelShapes.union(shapeRight, VoxelShapes.cuboid(0, 1.4375, 0.875, 1, 1.6875, 0.9375));
-        shapeRight = VoxelShapes.union(shapeRight, VoxelShapes.cuboid(0, 0.6875, 0.0625, 0.0625, 0.9375, 0.1875));
-        shapeRight = VoxelShapes.union(shapeRight, VoxelShapes.cuboid(0, 0.9375, 0.0625, 0.0625, 1, 0.875));
+        var vr1 = VoxelShapes.cuboid(0, 0, 0.0625, 0.0625, 0.625, 0.1875);
+        var vr2 = VoxelShapes.cuboid(0, 0, 0.8125, 0.0625, 0.625, 0.9375);
+        var vr3 = VoxelShapes.cuboid(0, 0.625, 0.0625, 1, 0.6875, 0.9375);
+        var vr4 = VoxelShapes.cuboid(0, 0.6875, 0.875, 0.0625, 1.4375, 0.9375);
+        var vr5 = VoxelShapes.cuboid(0.9375, 0.6875, 0.875, 1, 1.4375, 0.9375);
+        var vr6 = VoxelShapes.cuboid(0.6875, 0.6875, 0.875, 0.8125, 1.4375, 0.9375);
+        var vr7 = VoxelShapes.cuboid(0.4375, 0.6875, 0.875, 0.5625, 1.4375, 0.9375);
+        var vr8 = VoxelShapes.cuboid(0.1875, 0.6875, 0.875, 0.3125, 1.4375, 0.9375);
+        var vr9 = VoxelShapes.cuboid(0, 1.4375, 0.875, 1, 1.6875, 0.9375);
+        var vr10 = VoxelShapes.cuboid(0, 0.6875, 0.0625, 0.0625, 0.9375, 0.1875);
+        var vr11 = VoxelShapes.cuboid(0, 0.9375, 0.0625, 0.0625, 1, 0.875);
+
+        VoxelShape shapeRight = VoxelShapes.union(vr1,vr2,vr3,vr4,vr5,vr6,vr7,vr8,vr9,vr10,vr11);
 
         SHAPES_RIGHT.put(Direction.NORTH, Utils.rotateShape(Direction.NORTH, shapeRight));
         SHAPES_RIGHT.put(Direction.SOUTH, Utils.rotateShape(Direction.SOUTH, shapeRight));
         SHAPES_RIGHT.put(Direction.EAST, Utils.rotateShape(Direction.EAST, shapeRight));
         SHAPES_RIGHT.put(Direction.WEST, Utils.rotateShape(Direction.WEST, shapeRight));
 
-        VoxelShape shapeNone = VoxelShapes.empty();
-        shapeNone = VoxelShapes.union(shapeNone, VoxelShapes.cuboid(0, 0.625, 0.0625, 1, 0.6875, 0.9375));
-        shapeNone = VoxelShapes.union(shapeNone, VoxelShapes.cuboid(0, 0.6875, 0.875, 0.0625, 1.4375, 0.9375));
-        shapeNone = VoxelShapes.union(shapeNone, VoxelShapes.cuboid(0.9375, 0.6875, 0.875, 1, 1.4375, 0.9375));
-        shapeNone = VoxelShapes.union(shapeNone, VoxelShapes.cuboid(0.6875, 0.6875, 0.875, 0.8125, 1.4375, 0.9375));
-        shapeNone = VoxelShapes.union(shapeNone, VoxelShapes.cuboid(0.4375, 0.6875, 0.875, 0.5625, 1.4375, 0.9375));
-        shapeNone = VoxelShapes.union(shapeNone, VoxelShapes.cuboid(0.1875, 0.6875, 0.875, 0.3125, 1.4375, 0.9375));
-        shapeNone = VoxelShapes.union(shapeNone, VoxelShapes.cuboid(0, 1.4375, 0.875, 1, 1.6875, 0.9375));
+        var vn1 = VoxelShapes.cuboid(0, 0.625, 0.0625, 1, 0.6875, 0.9375);
+        var vn2 = VoxelShapes.cuboid(0, 0.6875, 0.875, 0.0625, 1.4375, 0.9375);
+        var vn3 = VoxelShapes.cuboid(0.9375, 0.6875, 0.875, 1, 1.4375, 0.9375);
+        var vn4 = VoxelShapes.cuboid(0.6875, 0.6875, 0.875, 0.8125, 1.4375, 0.9375);
+        var vn5 = VoxelShapes.cuboid(0.4375, 0.6875, 0.875, 0.5625, 1.4375, 0.9375);
+        var vn6 = VoxelShapes.cuboid(0.1875, 0.6875, 0.875, 0.3125, 1.4375, 0.937);
+        var vn7 = VoxelShapes.cuboid(0, 1.4375, 0.875, 1, 1.6875, 0.9375);
+
+        VoxelShape shapeNone = VoxelShapes.union(vn1, vn2, vn3, vn4, vn5, vn6, vn7);
 
         SHAPES_NONE.put(Direction.NORTH, Utils.rotateShape(Direction.NORTH, shapeNone));
         SHAPES_NONE.put(Direction.SOUTH, Utils.rotateShape(Direction.SOUTH, shapeNone));
