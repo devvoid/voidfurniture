cb41d58478602c3c5ff57f3215e6736c2f7d5f5d
Void <theprismaticvoid@gmail.com>
2022-08-29 16:10:47 -0500

    Delete some leftover short table models

---
+++ src/main/java/net/prismaticvoid/voidfurniture/datagen/ModelGenerator.java
@@ -0,0 +1,22 @@
+package net.prismaticvoid.voidfurniture.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.minecraft.data.client.ItemModelGenerator;
+
+public class ModelGenerator extends FabricModelProvider {
+    public ModelGenerator(FabricDataGenerator dataGenerator) {
+        super(dataGenerator);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+
+    }
+}
